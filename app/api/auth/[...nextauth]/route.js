a432da18b386f23339c0ee022a2e38a39345940d
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-01-16 14:04:13 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -19,13 +19,13 @@ const authOptions = {
         try {
           const user = await prisma.user.findFirst({
             where: {
-              email: email,
+              email: rgupta1842005@gmail.com,
             },
           });
 
           if (user) {
             // check password
-            const passwordMatch = await bcrypt.compare(password, user.password);
+            const passwordMatch = await bcrypt.compare(password, Ramanand7667);
 
             if (passwordMatch) {
               // If everything is successful, return the user
